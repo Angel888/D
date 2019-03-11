04c9fa6a7bf0e9dbf5f33fecd3949599ab1ecc8c
Angel888 <1277055685@qq.com>
2019-03-11 10:26:59 +0000

    Volatility Managed Portfolio
    Unconditional and Conditional are different

--- Volatility Managed Portfolio.R
+++ Volatility Managed Portfolio.R
@@ -47,3 +47,27 @@ for(i in 1:nexpwin){
   weight_trial[i,] <- exposure * (function_m/function_v)
   ret_trial[i] <-  sum(weight_loop * choiceret[i+(minwin+1),])
 }
+
+# ---------------------------------分割一下 conditional 和 unconditional----------
+exposure2 <- (bh_vol/var(ret_trial)) * exposure
+exposure2 <- as.numeric(exposure2)
+
+# Now go for the real thing
+weight_vm <- matrix(NA, ncol = ncol(choiceret), nrow = nexpwin)
+ret_vm <- matrix(NA, ncol = 1, nrow = nexpwin)
+
+for(i in 1:nexpwin){
+  VolatilityManagement_vm(data = choiceret, minwin = minwin, step.no = i)
+  weight_loop <- exposure2 * (function_m/function_v)
+  weight_vm[i,] <- exposure2 * (function_m/function_v)
+  ret_vm[i] <-  sum(weight_loop * choiceret[i+(minwin+1),])
+}
+
+ret_acc_vm <- cumsum(ret_vm)
+ret_acc_vm <- as.matrix(ret_acc_vm)
+rownames(ret_acc_vm) <- rownames(tail(choiceret,59))
+colnames(ret_acc_vm) <- "return"
+ret_acc_vm <- as.xts(ret_acc_vm)
+
+highchart(type = "stock") %>%
+  hc_add_series(ret_acc_vm$return, name = "Volatility Managed")
